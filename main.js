428c5aa1cc2ffea91febd78437b4ccf732e777ed
wxyzcctv <2846629226@qq.com>
2019-02-18 21:05:27 +0800

    使用Vue实现

--- main.js
+++ main.js
@@ -1,32 +1,22 @@
 fakeData()
 
-function Model(options){
+function Model(options) {
     this.data = options.data
-    this.resouce = options.resouce
+    this.resource = options.resource
 }
-Model.prototype.fetch = function(id){
-    return axios.get(`/${this.resouce}s/${id}`).then((response) => {
+Model.prototype.fetch = function (id) {
+    return axios.get(`/${this.resource}s/${id}`).then((response) => {
         this.data = response.data
         return response
     })
 }
-Model.prototype.updata = function(id,data){
-    return axios.put(`/${this.resouce}s/${id}`, data).then((response) => {
+Model.prototype.update = function (id, data) {
+    return axios.put(`/${this.resource}s/${id}`, data).then((response) => {
         this.data = response.data
         return response
     })
 }
-function View({el,template}){
-    this.el = el
-    this.template = template
-}
-View.prototype.render = function(data){
-    let html = this.template
-    for(let key in data){
-        html = html.replace(`__${key}__`,data[key])
-    }
-    $(this.el).html(html)
-}
+
 //----------上面是MVC类，下面是对象
 
 let model = new Model({
@@ -35,67 +25,66 @@ let model = new Model({
         number: 0,
         id: ''
     },
-    resouce:'book'
+    resource: 'book'
 })
 
-let view = new View({
+let view = new Vue({
     el: '#app',
+    data: {
+        book: {
+            name: '未命名',
+            number: 0,
+            id: ''
+        },
+        n:1
+    },
     template: `
     <div>
-        书名：《__name__》
-        数量：<span id="number">__number__</span>
+        <div>
+            书名：《{{book.name}}》
+            数量：<span id="number">{{book.number}}</span>
+        </div>
+        <div>
+            <input v-model="n"> 
+            N 的值是{{n}}
+        </div>
+        <div>
+            <button v-on:click="addOne">加N</button>
+            <button v-on:click="minusOne">减N</button>
+            <button v-on:click="reset">归零</button>
+      </div>
     </div>
-    <div>
-        <button id="addOne">加1</button>
-        <button id="minusOne">减1</button>
-        <button id="reset">归零</button>
-    </div>
-    `
-})
-
-var controller = {
-    init(options) {
-        let view = options.view
-        let model = options.model
-        this.view = view
-        this.model = model
-        this.view.render(this.model.data)
-        this.bindEvents()
-        this.model.fetch(1)
-            .then(() => {
-                this.view.render(this.model.data)
+    `,
+    created() {
+        model.fetch(1).then(() => {
+            this.book = model.data
+        })
+    },
+    methods: {
+        addOne() {
+            model.update(1, {
+                number: this.book.number + (this.n - 0)
+            }).then(() => {
+                this.view.book = this.model.data
             })
-    },
-    addOne() {
-        var oldNumber = $('#number').text()//获取到的是字符串string
-        var newNumber = oldNumber - 0 + 1//先减0是为了将string变为数值
-        this.model.updata(1, { number: newNumber }).then(({ data }) => {
-            this.view.render(this.model.data)
-        })
-        //点击之后发送一个普通请求到后端，并且传入改动的数据，如果发送成功，前端才进行新的数据显示
-        //将获取到的数据作为字符串的形式传给显示的部分
-    },
-    minusOne() {
-        var oldNumber = $('#number').text()
-        var newNumber = oldNumber - 0 - 1
-        this.model.updata(1, { number: newNumber }).then(() => {
-            this.view.render(this.model.data)
-        })
-    },
-    reset() {
-        this.model.updata(1, { number: 0 }).then(() => {
-            this.view.render(this.model.data)
-        })
-    },
-    bindEvents() {
-        $(this.view.el).on('click', '#addOne', this.addOne.bind(this))
-        $(this.view.el).on('click', '#minusOne', this.minusOne.bind(this))
-        $(this.view.el).on('click', '#reset', this.reset.bind(this))
+
+        },
+        minusOne() {
+            model.update(1, {
+                number: this.book.number - (this.n - 0)
+            }).then(() => {
+                this.view.book = this.model.data
+            })
+        },
+        reset() {
+            model.update(1, {
+                number: 0
+            }).then(() => {
+                this.view.book = this.model.data
+            })
+        },
     }
-}
-controller.init({ view: view, model: model })
-
-
+})
 
 
 //*********//
